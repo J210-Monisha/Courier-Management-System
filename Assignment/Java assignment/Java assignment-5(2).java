84a8e84e275bb1caa67a72e11f70f7b23d7b2b2d
J210-Monisha <m.monishadoss@gmail.com>
2025-04-05 17:47:09 +0530

    Add files via upload

---
+++ Assignment/Java assignment/Java assignment-5(2).java
@@ -0,0 +1,34 @@
+package entities;
+public class CourierServices {
+    private int serviceID;
+    private String serviceName;
+    private double cost;
+
+    public CourierServices() {}
+
+    public CourierServices(int serviceID, String serviceName, double cost) {
+        this.serviceID = serviceID;
+        this.serviceName = serviceName;
+        this.cost = cost;
+    }
+
+    public int getServiceID() { return serviceID; }
+    public void setServiceID(int serviceID) { this.serviceID = serviceID; }
+
+    public String getServiceName() { return serviceName; }
+    public void setServiceName(String serviceName) { this.serviceName = serviceName; }
+
+    public double getCost() { return cost; }
+    public void setCost(double cost) { this.cost = cost; }
+
+    @Override
+    public String toString() {
+        return "CourierServices [serviceID=" + serviceID + ", serviceName=" + serviceName + ", cost=" + cost + "]";
+    }
+
+    //for testing
+    public static void main(String[] args) {
+        CourierServices cs = new CourierServices(1, "Express Delivery", 299.99);
+        System.out.println(cs);
+    }
+}
